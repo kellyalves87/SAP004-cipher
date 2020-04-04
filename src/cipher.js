9d3e5c6c7a33ade2c74ae8ea2f8fd90c96e0a4b2
kelly <kellyp.alves87@gmail.com>
2020-04-04 18:47:17 -0300

    Alteração no type number para não ter opção de numero negativo

--- src/cipher.js
+++ src/cipher.js
@@ -2,49 +2,49 @@ const cipher = {
 
   encode: (offset, phrase) => {
 
-  let done = "";
+    let done = "";
 
-  for (let contador = 0; contador < phrase.length; contador++) {
+    for (let contador = 0; contador < phrase.length; contador++) {
 
-    let encodeChar = phrase[contador].toUpperCase().charCodeAt();
+      let encodeChar = phrase[contador].toUpperCase().charCodeAt();
 
-    let result = ((encodeChar - 65 + offset) % 26) + 65;  
+      let result = ((encodeChar - 65 + offset) % 26) + 65;
 
-    done += String.fromCharCode(result);
+      done += String.fromCharCode(result);
+
+    }
+
+    return done;
+  },
+
+
+  //função criada para decodificar.
+
+  decode: (offset, phrase) => {
+
+    let normalAlpha = phrase.toUpperCase();
+    let doneD = "";
+
+    for (let contador = 0; contador < phrase.length; contador++) {
+
+      let decodeChar = normalAlpha[contador].charCodeAt();
+
+      let resultD = ((decodeChar - 65 - offset) % 26);
+
+      if (resultD < 0) {
+        resultD += 26 + 65;
+        doneD += String.fromCharCode(resultD);
+      } else {
+        resultD += 65;
+        doneD += String.fromCharCode(resultD);
+      }
+
+    }
+
+    return doneD;
 
   }
 
-  return done;
-},
-
-
-//função criada para decodificar.
-
-decode: (offset, phrase) => {
-
-  let normalAlpha = phrase.toUpperCase();
-  let doneD = "";
-
-  for (let contador = 0; contador < phrase.length; contador++){
-   
-    let decodeChar = normalAlpha[contador].charCodeAt();
-    
-    let resultD = ((decodeChar - 65 -  offset) % 26);
-
-    if (resultD < 0){
-      resultD += 26 + 65;
-      doneD += String.fromCharCode(resultD);
-    } else {
-      resultD +=65;
-      doneD += String.fromCharCode(resultD);
-    }    
-     
-  }
-  
-  return doneD;
-
-  }
-  
 }
 
 export default cipher;
