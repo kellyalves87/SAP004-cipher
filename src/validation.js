97d2b28d747b157179caae33abb8a669df31821a
kelly <kellyp.alves87@gmail.com>
2020-04-06 14:54:07 -0300

    alteração da tag title e inserção de mensagem de alerta nos inputs text e number

--- src/validation.js
+++ src/validation.js
@@ -2,11 +2,11 @@ let validation = (offset, phrase) => {
 
     let message = document.getElementById('phrasePass');
     let moveOffset = document.getElementById('moveHouses');
-    
+
     message.innerHTML = "";
     moveOffset.innerHTML = "";
 
-    if ((offset <= 0) || phrase === 'undefined' || phrase === 'null' || phrase.length === 0) {
+    if ((Number.isNaN(offset) || offset <= 0) || phrase === 'undefined' || phrase === 'null' || phrase.length === 0) {
         message.innerHTML = "Campo obrigatório";
         moveOffset.innerHTML = "Campo obrigatório";
         throw new TypeError('Campo inválido!');
