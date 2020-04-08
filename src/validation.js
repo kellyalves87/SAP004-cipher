fe88c41e74e77e3bd670c4ccb0bd3f584cad9cc2
kelly <kellyp.alves87@gmail.com>
2020-04-08 09:48:24 -0300

    retirada de mensagem de alerta da validação

--- src/validation.js
+++ src/validation.js
@@ -1,21 +1,8 @@
 let validation = (offset, phrase) => {
 
-    let message = document.getElementById('phrasePass');
-    let moveOffset = document.getElementById('moveHouses');
-
-    message.innerHTML = "";
-    moveOffset.innerHTML = "";
-
-    if ((Number.isNaN(offset) || offset <= 0) || phrase === 'undefined' || phrase === 'null' || phrase.length === 0) {
-        message.innerHTML = "Campo obrigatório";
-        moveOffset.innerHTML = "Campo obrigatório";
+    if ((Number.isNaN(offset) || offset <= 0) || phrase === 'undefined' || phrase === 'null' || phrase.length === 0) {        
         throw new TypeError('Campo inválido!');
-    } else {
-        message.innerHTML = "";
-        moveOffset.innerHTML = "";
     }
-}
-
- 
+} 
 
 export default validation;
