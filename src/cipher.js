c94c427a73367e23029948b5f929ab4bd6c415e0
kelly <kellyp.alves87@gmail.com>
2020-04-10 13:42:31 -0300

    mudança em nome de variável contador para counter

--- src/cipher.js
+++ src/cipher.js
@@ -8,9 +8,10 @@ const cipher = {
 
     let done = "";
 
-    for (let contador = 0; contador < phrase.length; contador++) {
+    for (let counter = 0; counter < phrase.length; counter++) {
 
-      let encodeChar = phrase[contador].charCodeAt();
+      let encodeChar = phrase[counter].charCodeAt();
+
       if (encodeChar >= 65 && encodeChar <= 90) {
         let result = ((encodeChar - 65 + offset) % 26) + 65;
         done += String.fromCharCode(result);
@@ -22,24 +23,24 @@ const cipher = {
     }
     return done;
   },
-
-
-  //função criada para decodificar em letra maíuscula e minuscula.
+  
+     //função criada para decodificar em letra maíuscula e minuscula.
 
   decode: (offset, phrase) => {
 
     let normalAlpha = phrase;
     let doneD = "";
 
-    for (let contador = 0; contador < phrase.length; contador++) {
+    for (let counter = 0; counter < phrase.length; counter++) {
 
-      let decodeChar = normalAlpha[contador].charCodeAt();
+      let decodeChar = normalAlpha[counter].charCodeAt();
+
       if (decodeChar >= 65 && decodeChar <= 90) {
         let resultD = ((decodeChar + 65 - offset) % 26) + 65;
         doneD += String.fromCharCode(resultD);
       } else if (decodeChar >= 97 && decodeChar <= 122) {
 
-        let upperDecodeChar = normalAlpha[contador].toUpperCase().charCodeAt();
+        let upperDecodeChar = normalAlpha[counter].toUpperCase().charCodeAt();
         let resultD = ((upperDecodeChar + 65 - offset) % 26) + 65;
         doneD += String.fromCharCode(resultD).toLowerCase();
         
