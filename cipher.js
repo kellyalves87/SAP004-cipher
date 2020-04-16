bd3c3a27a40a7f937f0584ba05e7c546e02d3ff1
kelly <kellyp.alves87@gmail.com>
2020-04-16 14:33:36 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -1,57 +1,52 @@
-import validation from './validation.js';
+import validation from "./validation.js";
 
 const cipher = {
-
   encode: (offset, phrase) => {
-
     validation(offset, phrase);
 
+    let firsMintLetterAsc = 65;
+    let lastMinLetterAsc = 90;
+    let fisrtMaxtLetterAsc = 97;
+    let numberLetterAlpha = 26;
     let done = "";
 
     for (let counter = 0; counter < phrase.length; counter++) {
-
       const encodeChar = phrase[counter].charCodeAt();
 
-      if (encodeChar >= 65 && encodeChar <= 90) {
-        const result = ((encodeChar - 65 + offset) % 26) + 65;
+      if (encodeChar >= firsMintLetterAsc && encodeChar <= lastMinLetterAsc) {
+        const result = ((encodeChar - firsMintLetterAsc + offset) % numberLetterAlpha) + firsMintLetterAsc;
         done += String.fromCharCode(result);
       } else {
-        const result = ((encodeChar - 97 + offset) % 26) + 97;
+        const result = ((encodeChar - fisrtMaxtLetterAsc + offset) % numberLetterAlpha) + fisrtMaxtLetterAsc;
         done += String.fromCharCode(result);
       }
-
     }
     return done;
   },
-  
-  
-  decode: (offset, phrase) => {
 
+  decode: (offset, phrase) => {
     validation(offset, phrase);
 
+    let firsMintLetterAsc = 65;
+    let lastMinLetterAsc = 90;
+    let numberLetterAlpha = 26;
     let done = "";
 
     for (let counter = 0; counter < phrase.length; counter++) {
-
       const decodeChar = phrase[counter].charCodeAt();
 
-      if (decodeChar >= 65 && decodeChar <= 90) {
-        const result = ((decodeChar + 65 - offset) % 26) + 65;
+      if (decodeChar >= firsMintLetterAsc && decodeChar <= lastMinLetterAsc) {
+        const result = ((decodeChar + firsMintLetterAsc - offset) % numberLetterAlpha) + firsMintLetterAsc;
         done += String.fromCharCode(result);
       } else {
-
         const upperDecodeChar = phrase[counter].toUpperCase().charCodeAt();
-        const result = ((upperDecodeChar + 65 - offset) % 26) + 65;
+        const result = ((upperDecodeChar + firsMintLetterAsc - offset) % numberLetterAlpha) + firsMintLetterAsc;
         done += String.fromCharCode(result).toLowerCase();
-        
       }
-
     }
 
     return done;
-
-  }
-
-}
+  },
+};
 
 export default cipher;
